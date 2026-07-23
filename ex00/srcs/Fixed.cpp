f26e257994c9d565a4834aae7682846c7b5bcfcd
pmalumba <pmalumba@f4r13s3.paris.42.school>
2026-07-23 16:19:21 +0200

    42

--- ex00/srcs/Fixed.cpp
+++ ex00/srcs/Fixed.cpp
@@ -6,14 +6,13 @@
 /*   By: pmalumba <pmalumba@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/07/22 18:07:52 by pmalumba          #+#    #+#             */
-/*   Updated: 2026/07/22 18:07:53 by pmalumba         ###   ########.fr       */
+/*   Updated: 2026/07/23 16:02:48 by pmalumba         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/Fixed.hpp"
 
-Fixed::Fixed(){
-    _fvalue = 0;
+Fixed::Fixed() : _fvalue(0) {
     std::cout << "Default constructor called" << std::endl;
 }
 
